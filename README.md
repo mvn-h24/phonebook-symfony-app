8d08456e83acb34f73bb6858e68fbc14b07763ad
mvn-h24 <morning.vecher.night@yandex.ru>
2022-05-18 18:36:49 +0300

    add fixtures, php-unit with api-test utils

--- README.md
+++ README.md
@@ -8,5 +8,5 @@ zsh .docker/scripts/dev.start.sh #db with web ui start
 cp .env.dev .env.local
 symfony composer install
 symfony console doctrine:migrations:migrate
-symfony serve # ready to testing
+symfony console hautelook:fixtures:load -q --no-bundles
 ```
--- config/bundles.php
+++ config/bundles.php
@@ -8,4 +8,7 @@ return [
     ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    Nelmio\Alice\Bridge\Symfony\NelmioAliceBundle::class => ['dev' => true, 'test' => true],
+    Fidry\AliceDataFixtures\Bridge\Symfony\FidryAliceDataFixturesBundle::class => ['dev' => true, 'test' => true],
+    Hautelook\AliceBundle\HautelookAliceBundle::class => ['dev' => true, 'test' => true],
 ];
